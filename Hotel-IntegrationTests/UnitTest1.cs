5c2ee95a610e510cfb3e770f656270acd631c6b2
Kiyomarss <kiyomarss.p1997@gmail.com>
2024-12-27 22:26:26 +0330

    قایل های اولیه تست نرم‌افزار

---
+++ Hotel-IntegrationTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel_IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Hotel-ServiceTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel_ServiceTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
